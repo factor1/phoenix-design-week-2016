28aeecfe5fe1486d0bac2a5c09e02383edea5a26
Eric Stout <ericwstout@gmail.com>
2016-04-08 11:15:08 -0700

    Adds print grid, adds footer

--- templates/splash.php
+++ templates/splash.php
@@ -207,6 +207,7 @@ get_header();
         </h1>
       </div>
     </div>
+
     <?php // Premier Sponsors ?>
     <div class="row">
       <div class="col-12 text-center">
@@ -218,7 +219,7 @@ get_header();
     <?php if(have_rows('premier_sponsors', 'option')):?>
       <div class="row">
         <?php while(have_rows('premier_sponsors', 'option')): the_row();?>
-          <div class="col-3 text-center">
+          <div class="col-3 text-center sponsors--grid-item">
             <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
               <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
             </a>
@@ -226,6 +227,27 @@ get_header();
         <?php endwhile;?>
       </div>
     <?php endif;?>
+
+    <?php // Print Sponsors ?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Print Sponsors
+        </h4>
+      </div>
+    </div>
+    <?php if(have_rows('print_sponsors', 'option')):?>
+      <div class="row">
+        <?php while(have_rows('print_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+
   </div>
 </section>
 
