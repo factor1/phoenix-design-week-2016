6dd9913b169576df92dd6d3a721a7d5f5b4cb403
Eric Stout <ericwstout@gmail.com>
2016-04-08 14:31:34 -0700

    Adds wrapper for products

--- woocomerce/content-single-product.php
+++ woocomerce/content-single-product.php
@@ -21,64 +21,70 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<?php
-	/**
-	 * woocommerce_before_single_product hook.
-	 *
-	 * @hooked wc_print_notices - 10
-	 */
-	 do_action( 'woocommerce_before_single_product' );
+<div class="container">
+	<div class="row">
+		<div class="col-10 col-centered">
+			<?php
+				/**
+				 * woocommerce_before_single_product hook.
+				 *
+				 * @hooked wc_print_notices - 10
+				 */
+				 do_action( 'woocommerce_before_single_product' );
 
-	 if ( post_password_required() ) {
-	 	echo get_the_password_form();
-	 	return;
-	 }
-?>
+				 if ( post_password_required() ) {
+				 	echo get_the_password_form();
+				 	return;
+				 }
+			?>
 
-<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php
-		/**
-		 * woocommerce_before_single_product_summary hook.
-		 *
-		 * @hooked woocommerce_show_product_sale_flash - 10
-		 * @hooked woocommerce_show_product_images - 20
-		 */
-		do_action( 'woocommerce_before_single_product_summary' );
-	?>
+				<?php
+					/**
+					 * woocommerce_before_single_product_summary hook.
+					 *
+					 * @hooked woocommerce_show_product_sale_flash - 10
+					 * @hooked woocommerce_show_product_images - 20
+					 */
+					do_action( 'woocommerce_before_single_product_summary' );
+				?>
 
-	<div class="summary entry-summary">
+				<div class="summary entry-summary">
 
-		<?php
-			/**
-			 * woocommerce_single_product_summary hook.
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-		?>
+					<?php
+						/**
+						 * woocommerce_single_product_summary hook.
+						 *
+						 * @hooked woocommerce_template_single_title - 5
+						 * @hooked woocommerce_template_single_rating - 10
+						 * @hooked woocommerce_template_single_price - 10
+						 * @hooked woocommerce_template_single_excerpt - 20
+						 * @hooked woocommerce_template_single_add_to_cart - 30
+						 * @hooked woocommerce_template_single_meta - 40
+						 * @hooked woocommerce_template_single_sharing - 50
+						 */
+						do_action( 'woocommerce_single_product_summary' );
+					?>
 
-	</div><!-- .summary -->
+				</div><!-- .summary -->
 
-	<?php
-		/**
-		 * woocommerce_after_single_product_summary hook.
-		 *
-		 * @hooked woocommerce_output_product_data_tabs - 10
-		 * @hooked woocommerce_upsell_display - 15
-		 * @hooked woocommerce_output_related_products - 20
-		 */
-		do_action( 'woocommerce_after_single_product_summary' );
-	?>
+				<?php
+					/**
+					 * woocommerce_after_single_product_summary hook.
+					 *
+					 * @hooked woocommerce_output_product_data_tabs - 10
+					 * @hooked woocommerce_upsell_display - 15
+					 * @hooked woocommerce_output_related_products - 20
+					 */
+					do_action( 'woocommerce_after_single_product_summary' );
+				?>
 
-	<meta itemprop="url" content="<?php the_permalink(); ?>" />
+				<meta itemprop="url" content="<?php the_permalink(); ?>" />
 
-</div><!-- #product-<?php the_ID(); ?> -->
+			</div><!-- #product-<?php the_ID(); ?> -->
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+			<?php do_action( 'woocommerce_after_single_product' ); ?>
+		</div>
+	</div>
+</div>
