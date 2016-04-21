2bd03578fe43303ad9aa9c1240950a1cb0e8ab9d
Eric Stout <ericwstout@gmail.com>
2016-04-21 16:47:54 -0700

    Adds button styles

--- parts/newsletter.php
+++ parts/newsletter.php
@@ -34,7 +34,7 @@
       	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_fb9deb0302_f1592b3c15" tabindex="-1" value=""></div>
         <div class="col-2">
-          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button button-drkpurple"></div>
         </div>
       </form>
     </div>
