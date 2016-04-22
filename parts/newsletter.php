b8e127fc32b01c665810212a87fac493796ae217
Eric Stout <ericwstout@gmail.com>
2016-04-22 11:39:39 -0700

    Adds confirmation element for form submits

--- parts/newsletter.php
+++ parts/newsletter.php
@@ -30,7 +30,7 @@
         </div>
       	<div id="mce-responses" class="clear">
       		<div class="response" id="mce-error-response" style="display:none"></div>
-      		
+      		<div class="response" id="mce-success-response" style="display:none"></div>
       	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_fb9deb0302_f1592b3c15" tabindex="-1" value=""></div>
         <div class="col-2">
