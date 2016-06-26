11736a0584be837fcfe5017c0fafef6825220f78
mattada <mattadams@factor1studios.com>
2016-06-26 09:21:48 -0700

    Sponsor / partner file flip
    Removed the worng file in a clean up. Partners were moved to be a part of sponsor template

---
+++ parts/sponsors.php
@@ -0,0 +1,72 @@
+<?php // Sponsors Container ?>
+<section class="sponsors--container">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>
+          PHXDW Is Made Possible By Generous Sponsors
+        </h1>
+      </div>
+    </div>
+
+    <?php // Premier Sponsors ?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Premier Sponsors
+        </h4>
+      </div>
+    </div>
+    <?php if(have_rows('premier_sponsors', 'option')):?>
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('premier_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+
+    <?php // Print Sponsors ?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Print Sponsors
+        </h4>
+      </div>
+    </div>
+    <?php if(have_rows('print_sponsors', 'option')):?>
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('print_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+           Partner Organizations who helped organize this event
+        </h4>
+      </div>
+    </div>
+    <?php if(have_rows('partner_sponsors', 'option')):?>
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('partner_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+
+  </div>
+</section>
