df9153060f699dabce7b736b9e3dec234c2cf911
mattada <mattadams@factor1studios.com>
2016-06-30 21:06:33 -0700

    Add adobe and GCU sponsors

--- parts/sponsors.php
+++ parts/sponsors.php
@@ -9,11 +9,34 @@
       </div>
     </div>
 
+    
     <?php // Premier Sponsors ?>
     <div class="row">
       <div class="col-12 text-center">
         <h4 class="sponsors--headline">
-          Premier Print Sponsors
+          Premier Sponsors
+        </h4>
+      </div>
+    </div>
+    <?php //if(have_rows('premier_sponsors', 'option')):?>
+      <div class="row row--justify-content-center">
+        <?php //while(have_rows('premier_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="http://adobe.com<?php //the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php //the_sub_field('sponsor_image');?>  <?php bloginfo('template_url');?>/assets/img/adobe.png" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php //endwhile;?>
+      </div>
+    <?php //endif;?>
+
+    
+    
+    <?php // Premier Sponsors ?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Premier Print And Large Format Sponsor
         </h4>
       </div>
     </div>
@@ -28,6 +51,29 @@
         <?php endwhile;?>
       </div>
     <?php endif;?>
+    
+    
+    <?php // Leadership Sponsors ?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Leadership Sponsors
+        </h4>
+      </div>
+    </div>
+    <?php //if(have_rows('premier_sponsors', 'option')):?>
+      <div class="row row--justify-content-center">
+        <?php //while(have_rows('premier_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="http://gcu.edu<?php //the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php //the_sub_field('sponsor_image');?>  <?php bloginfo('template_url');?>/assets/img/gcu.jpg" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php //endwhile;?>
+      </div>
+    <?php //endif;?>
+    
+    
 
     <?php // Print Sponsors ?>
     <div class="row">
