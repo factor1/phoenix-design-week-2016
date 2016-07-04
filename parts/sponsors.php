675c6587ae9a34de48ece092307ea638837f8521
mattada <mattadams@factor1studios.com>
2016-07-04 15:06:25 -0700

    Sponsor updates for new tiers
    Add all 6 total sponsor types + partner rows. Updated logic to only show the row IF the ACF content is there.

--- parts/sponsors.php
+++ parts/sponsors.php
@@ -1,6 +1,7 @@
 <!--
 Title Sponsor - Limit 1
 Premier Sponsor - Limit 5
+Premier Print and Large Format Sponsor - Limit 5
 Leadership Sponsor 
 Industry Sponsor
 Agency Sponsor
@@ -20,7 +21,9 @@ Agency Sponsor
       </div>
     </div>
 
-    <?php // Premier Sponsors ?>
+    <?php // Title Sponsors ?>
+    <?php if(have_rows('title_sponsors', 'option')):?>
+    
     <div class="row">
       <div class="col-12 text-center">
         <h4 class="sponsors--headline">
@@ -28,7 +31,34 @@ Agency Sponsor
         </h4>
       </div>
     </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('title_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+      
+    <?php endif;?>
+
+
+
+
+
+    <?php // Premier Sponsors ?>
     <?php if(have_rows('premier_sponsors', 'option')):?>
+    
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Premier Sponsors
+        </h4>
+      </div>
+    </div>
+    
       <div class="row row--justify-content-center">
         <?php while(have_rows('premier_sponsors', 'option')): the_row();?>
           <div class="col-3 text-center sponsors--grid-item">
@@ -39,16 +69,21 @@ Agency Sponsor
         <?php endwhile;?>
       </div>
     <?php endif;?>
+    
+    
+    
+    
 
     <?php // Print Sponsors ?>
+    <?php if(have_rows('print_sponsors', 'option')):?>
     <div class="row">
       <div class="col-12 text-center">
         <h4 class="sponsors--headline">
-          Print Sponsors
+          Premier Print and Large Format Sponsor
         </h4>
       </div>
     </div>
-    <?php if(have_rows('print_sponsors', 'option')):?>
+    
       <div class="row row--justify-content-center">
         <?php while(have_rows('print_sponsors', 'option')): the_row();?>
           <div class="col-3 text-center sponsors--grid-item">
@@ -60,6 +95,87 @@ Agency Sponsor
       </div>
     <?php endif;?>
     
+    
+    
+
+
+    <?php // Leadership Sponsors ?>
+    <?php if(have_rows('leadership_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Industry Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('leadership_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+    
+    
+
+
+
+    <?php // Industry Sponsors ?>
+    <?php if(have_rows('industry_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Industry Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('industry_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+    
+    
+    <?php // Agency Sponsors ?>
+    <?php if(have_rows('agency_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Agency Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('agency_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+    
+    <?php // Partner Organization / Sponsors ?>
+    
+    <?php if(have_rows('partner_sponsors', 'option')):?>
     <div class="row">
       <div class="col-12 text-center">
         <h4 class="sponsors--headline">
@@ -67,7 +183,7 @@ Agency Sponsor
         </h4>
       </div>
     </div>
-    <?php if(have_rows('partner_sponsors', 'option')):?>
+    
       <div class="row row--justify-content-center">
         <?php while(have_rows('partner_sponsors', 'option')): the_row();?>
           <div class="col-3 text-center sponsors--grid-item">
