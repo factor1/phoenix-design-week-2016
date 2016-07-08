7de05b032c2d39eb3718d354138978a0f5248aba
mattada <mattadams@factor1studios.com>
2016-07-08 15:49:33 -0700

    Content add in
    Signed-off-by: mattada <mattadams@factor1studios.com>

--- templates/mm-conference.php
+++ templates/mm-conference.php
@@ -12,9 +12,11 @@
 
 <section class="hero container">
 	<div class="row hero-content text-center">
-		<h1 class="text-white">PHOENIX DESIGN WEEK IS COMING</h1>
-		<h3 class="text-white">October 22-28, 2016  •  Phoenix Design Week 2016</h3>
-		<a href="" class="button white text-purple">Claim your spot</a>
+		<h1 class="text-white">Method + Madness Conference</h1>
+		<h3 class="text-white">October 22-­23, 2016
+													<br>The Phoenix Convention Center
+													<br>8am Check­in • 9am Talks begin</h3>
+		<a href="" class="button white text-purple">Be There</a>
 	</div>
 </section>
 
@@ -76,8 +78,11 @@
 	<div class="row">
 		<div class="col-12">
 			<blockquote>
-			<h2  class="text-center">Some big quote here</h2>
-			<cite>Your momma</cite>
+			<h2  class="text-center">
+					"Method + Madness inspires me to create more, 
+					strive to make my work better, and try new things. 
+					It reminds me why I love design."</h2>
+					<cite>Kristie Erkkila</cite>
 			</blockquote>
 		</div>
 	</div>
@@ -90,6 +95,10 @@
 //============ ** ============ //
 ?>		
 
+<h2>Join us in downtown Phoenix, AZ</h2>
+<p>M + M takes place in the heart of the city. The increasingly revitalized downtown area is full of plenty of dining options and nightlife hotspots, and by October we’ll be experiencing some of the best weather of the year. </p>
+
+	Button text: Learn About Downtown Phoenix
 
 
 
@@ -101,16 +110,20 @@
 
 <section class="container halfsplit">
 	<div class="leftyleft">
-			<div class="thecontent">kdhsjf</div>
+			<div class="thecontent">
+			<h2>How does Method + Madness compare to other conferences?</h2>
+			<p>Not all conferences are created equal. Here’s why you should definitely attend this one.</p>
+			<a href="" class="button">SEE HOW M + M STACKS UP</a>
+			</div>
 	</div>
 	
 		<div class="rightyright stretch">
-			<div class="content-a">
-				<div class="thecontent">kdhsjf</div>
-			</div>
-			<div class="content-b">
-					<div class="thecontent">kdhsjf</div>
-			</div>
+				<div class="thecontent">
+				<h2>Need help convincing your boss?</h2>
+				<p>The Method + Madness Conference is a solid investment in your design career.</p>
+				<a href="" class="button">WHY YOU SHOULD ATTEND</a>
+				</div>
+
 	</div>
 </section>
 
@@ -176,6 +189,19 @@
 //  Students  (Phase 2)
 //============ ** ============ //
 ?>	
+
+
+<!-- left -->
+<h2>Are you a student on a shoestring?</h2>
+<p>Every year, we offer a limited number of student scholarships to attend the M + M Conference. </p>
+<a href="" class="button">LEARN MORE</a>
+
+
+<!-- right -->
+<h2>Why students love M + M</h2>
+<p>Don’t consider yourself a professional yet? We don’t mind! Here's why you should attend.</p>
+<a href="" class="button">GET THE DETAILS</a>
+
 
 
 
