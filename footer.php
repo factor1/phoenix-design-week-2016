ce26889fc2a3cf47f59c4cc137759441c0668c6e
Eric Stout <ericwstout@gmail.com>
2016-07-11 11:45:16 -0700

    Adds social row to footer with headline style adjustments

--- footer.php
+++ footer.php
@@ -3,10 +3,17 @@
 
 <footer class="container">
   <div class="row">
+    <div class="col-12 text-center social-icons">
+      <h3>
+        Follow us and sahre the #PHXDW love!
+      </h3>
+    </div>
+  </div>
+  <div class="row">
     <div class="col-12 text-center">
       <p>
         Phoenix Design Week was founded by <a href="http://www.lostcreature.com/">Lost Creature</a> and is organized by
-        <a href="http://arizona.aiga.org/">AIGA Arizona</a>. 
+        <a href="http://arizona.aiga.org/">AIGA Arizona</a>.
       </p>
     </div>
   </div>
