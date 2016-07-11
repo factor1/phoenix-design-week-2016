59be8c21322cb8f8c35e54cae138387a557846a6
Eric Stout <ericwstout@gmail.com>
2016-07-11 11:03:00 -0700

    Updates newsletter structure and button classes

--- parts/newsletter.php
+++ parts/newsletter.php
@@ -14,7 +14,7 @@
     </div>
 
     <!-- Begin MailChimp Signup Form -->
-    <div id="mc_embed_signup">
+    <div id="mc_embed_signup" class="container">
       <form action="//aiga.us5.list-manage.com/subscribe/post?u=fb9deb0302&amp;id=f1592b3c15" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate row row--justify-content-center" target="_blank" novalidate>
         <!-- <div class="mc-field-group col-5">
           <input type="text" value="" name="MMERGE5" class="" id="mce-MMERGE5" placeholder="Name">
@@ -28,7 +28,7 @@
       	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_fb9deb0302_f1592b3c15" tabindex="-1" value=""></div>
         <div class="col-2">
-          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button button-drkpurple"></div>
+          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button pink"></div>
         </div>
       </form>
     </div>
