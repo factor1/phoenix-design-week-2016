4fadc0d0a9419e1f931901194bbbef752035a23a
mattada <mattadams@factor1studios.com>
2016-07-11 20:53:17 -0700

    minor event updates

--- templates/events.php
+++ templates/events.php
@@ -14,49 +14,24 @@
 	<div class="row hero-content">
 		<div class="col-10 col-centered text-center">
 			<h1 class="text-white">
-				Method + Madness Conference
+				Phoenix Design Week Events
 			</h1>
-			<h3 class="text-white">
-				October 22-­23, 2016
-				<br>The Phoenix Convention Center
-				<br>8am Check­in • 9am Talks begin
-			</h3>
-			<a href="https://www.eventbrite.com/e/method-madness-conference-phxdw-2016-tickets-24939107542" class="button white text-purple">
-				Claim Your Spot
-			</a>
+			<h2 class="text-white">
+				Phoenix Design Week opens with the Kickoff Party and Method + Madness Conference, 
+				followed by an impressive series of events hosted by individual groups across Phoenix. 
+				See the current schedule below.
+			</p>
 		</div>
 	</div>
 </section>
 
-<?php
-//============ ** ============ //
-//  Second Row, info block
-//============ ** ============ //
-?>
-
-<section class="container conf-content">
-	<div class="row">
-	<article class="col-12">
-
-		<?php if(have_posts()):
-			while(have_posts()): the_post();
-				the_content();
-			endwhile;
-		endif ?>
-
-	</article>
-	</div>
-
-
-</section>
-
 
 
 
 <?php
-//============ ** ============ //
-//  Third Row, Event repeaters block
-//============ ** ============ //
+//================= ** ================= //
+//  Second Row, Event repeaters block
+//================= ** ================ //
 ?>
 
 <section class="container ">
@@ -79,13 +54,13 @@
 		
 		// No Events found content
 		
-		
+		/*
 		<p>Phoenix Design Week continues after Method + Madness with a series of daily events and
 		workshops throughout the metro Phoenix area. There are no events scheduled yet for this day.</p>
 		
 		<p><a href="#event_form">Want to get yours on the calendar?</a></p>
 		
-
+		*/  ?>
 
 	</article>
 	</div>
@@ -96,9 +71,9 @@
 
 
 <?php
-//============ ** ============ //
-// 	   Sign up for email
-//============ ** ============ //
+//=============== ** =============== //
+// 	   Suggest an Event form
+//=============== ** =============== //
 ?>
 
 <section class="container" id="event_form">
