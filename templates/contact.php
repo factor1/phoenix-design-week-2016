707764943b4fb3074a30042130794744225a24a5
mattada <mattadams@factor1studios.com>
2016-07-13 10:57:04 -0700

    contact sections

--- templates/contact.php
+++ templates/contact.php
@@ -5,12 +5,39 @@
 ?>
 
 
+<?php
+//============ ** ============ //
+// 			Page HERO
+//============ ** ============ //?>
 
+<section class="hero container">
+	<div class="row hero-content">
+		<div class="col-10 col-centered text-center">
+			<h1 class="text-white">
+				Phoenix Design Week Events
+			</h1>
+			<h2 class="text-white">
+				Phoenix Design Week opens with the Kickoff Party and Method + Madness Conference, 
+				followed by an impressive series of events hosted by individual groups across Phoenix. 
+				See the current schedule below.
+			</p>
+		</div>
+	</div>
+</section>
+
+
+
+<section class="container contactus">
+	<div class="row">
+	<div class="col-4">
+	
 		<?php if(have_posts()): 
 			while(have_posts()): the_post();
 				the_content();
 			endwhile; 
 		endif ?>
+		
+	</div>
 
 
 
@@ -20,7 +47,17 @@
 // 	   Gravity Form ID 1
 //============ ** ============ //
 
-FORMS!
+?>
+
+
+<div class="col-8">
+FORMS
+</div>
+
+	</div>
+</section>
+
+
 
 
 <?php 
