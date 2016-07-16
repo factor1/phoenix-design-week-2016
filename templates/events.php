4d064c91f1c2674e4164710b7939eddaff544516
mattada <mattadams@factor1studios.com>
2016-07-15 19:05:52 -0700

    Event Accordians - initial PHP add in

--- templates/events.php
+++ templates/events.php
@@ -37,6 +37,59 @@
 <section class="container weekevents">
 	<div class="row">
 	<article class="col-12">
+	
+	
+	
+	<?php
+	// Event Accordions
+	?>
+
+<?php if( have_rows('event_day') ):?>
+  <section class="flex-accordions container">
+
+    <?php while( have_rows('event_day') ): the_row(); ?>
+
+      <?php // ACCORDION TITLE ?>
+      <div class="accordion--title container">
+        <div class="row">
+          <div class="col-10 col-centered text-center">
+            <h1><?php the_sub_field('event_date');?></h1><i class="fa fa-chevron-down"></i>
+          </div>
+        </div>
+        <?php // ACCORDION CONTENT ?>
+        <div class="accordion--content">
+          <div class="container">
+            <div class="row">
+              <div class="col-10 col-centered text-center">
+			  	
+			  	<?php if( have_rows('events') ): while( have_rows('events') ): the_row(); ?>
+               	 
+		               	 <h4><?php the_sub_field('event_name'); ?></h4>
+		               	 
+		               	 <p><?php the_sub_field('event_date_location'); ?></p>
+		               	 <p><?php the_sub_field('event_detail'); ?></p>
+		               	 
+		               	 <a href="<?php the_sub_field('event_cta_url'); ?>" class="button">
+		               	 	<?php the_sub_field('event_button_text>'); ?>
+		               	 </a>
+                <?php endwhile; endif; ?>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <?php endwhile;?>
+
+  </section>
+<?php endif;?>
+
+
+
+
+
+
 
 		<?php
 		//  ACF Structure / Naming
