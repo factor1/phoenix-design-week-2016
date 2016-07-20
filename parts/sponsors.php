4fb64e1c618eec038f35e551c78cb5276d23239f
mattada <mattadams@factor1studios.com>
2016-07-20 14:29:16 -0700

    sponsor h2 color teal
    Signed-off-by: mattada <mattadams@factor1studios.com>

--- parts/sponsors.php
+++ parts/sponsors.php
@@ -15,7 +15,7 @@ Agency Sponsor
   <div class="container">
     <div class="row">
       <div class="col-12 text-center">
-        <h2>
+        <h2 class="text-teal">
           PHXDW Is Made Possible By Generous Sponsors
         </h2>
       </div>
