c9c0ad974bc24f568ec213a9a8ddb3c4298db68c
mattada <mattadams@factor1studios.com>
2016-07-21 21:22:27 -0700

    sponsor style tweaks
    Signed-off-by: mattada <mattadams@factor1studios.com>

--- templates/sponsor.php
+++ templates/sponsor.php
@@ -9,9 +9,9 @@
 		<div class="col-10 col-centered text-center">
 			<h1 class="text-white">Sponsor Phoenix Design Week</h1>
 	
-	<p>Phoenix Design Week is the premier design event in Arizona for designers, students, educators, writers, illustrators, and marketing professionals. Each year, over 450 individuals gather in downtown Phoenix to celebrate and share their skills, while another 4,000+ stay up to date on the event through social media and email. </p>
+	<p class="text-white">Phoenix Design Week is the premier design event in Arizona for designers, students, educators, writers, illustrators, and marketing professionals. Each year, over 450 individuals gather in downtown Phoenix to celebrate and share their skills, while another 4,000+ stay up to date on the event through social media and email. </p>
 	
-	<p>Between the two ­day Method + Madness Conference and a host of local events taking place
+	<p class="text-white">Between the two ­day Method + Madness Conference and a host of local events taking place
 	throughout the week, companies have plenty of opportunities to put their brand in front of an
 	excited, targeted audience. All that’s missing is you.</p>
 	
@@ -113,14 +113,18 @@
 //=============== ** =============== //
 ?>	
 
-<section class="contianer">
 
-<h2>Other sponsorship opportunities with AIGA</h2>
-<p>AIGA Arizona hosts over 25 events annually attended by thousands of Arizona designers. If you
+<section class="container home-sponsor">
+	<div class="row">
+		<div class="col-10 col-centered text-center">
+			<h3 class="text-white">Other sponsorship opportunities with AIGA</h3>
+			<p class="text-white">AIGA Arizona hosts over 25 events annually attended by thousands of Arizona designers. If you
 would like to support AIGA Arizona directly, there are multiple opportunities to interact with the
 Arizona design community year­ round. Please contact Danielle Knotts at
-danielle.knotts@arizona.aiga.org or (480) 529­1825 for more information.</p>
-
+<a href="mailto:danielle.knotts@arizona.aiga.org">danielle.knotts@arizona.aiga.org</a> or (480) 529­1825 for more information.</p>
+	
+		</div>
+	</div>
 </section>
 
 
