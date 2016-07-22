65327adb9213be031044d2c948a7c811847739da
mattada <mattadams@factor1studios.com>
2016-07-21 21:46:03 -0700

    default page template
    Signed-off-by: mattada <mattadams@factor1studios.com>

--- page.php
+++ page.php
@@ -7,42 +7,45 @@
   get_header();
 ?>
 
-<section class="page--intro">
-  <div class="container">
-    <div class="row">
-      <div class="col-8 col-centered text-center">
-        <a href="<?php bloginfo('url');?>/">
-          <img src="<?php bloginfo('template_url');?>/assets/img/phxdw-logo.png" alt="Phoenix Design Week 2016" class="main-logo">
-        </a>
-        <h1 class="event-date">
-          October 21-28, 2016
-        </h1>
-      </div>
-    </div>
-  </div>
+
+<?php
+//============ ** ============ //
+// 			Page HERO
+//============ ** ============ //?>
+
+<section class="hero container">
+	<div class="row hero-content">
+		<div class="col-10 col-centered text-center">
+			<h1 class="text-white">
+				<?php the_title();?>
+			</h1>
+		</div>
+	</div>
 </section>
 
-<section class="page--title container">
-  <div class="row">
-    <div class="col-10 col-centered text-center">
-      <h1>
-        <?php the_title();?>
-      </h1>
-    </div>
-  </div>
+
+
+<?php
+//============ ** ============ //
+// 			Page copy
+//============ ** ============ //?>
+
+<section class="container">
+	<div class="rowt">
+	<?php if(have_posts()):?>
+	  <section class="page--content container">
+	    <?php while(have_posts()): the_post();?>
+	      <div class="row">
+	        <article class="col-11 col-centered">
+	          <?php the_content();?>
+	        </article>
+	      </div>
+	    <?php endwhile;?>
+	  </section>
+	<?php endif;?>
+	</div>
 </section>
 
-<?php if(have_posts()):?>
-  <section class="page--content container">
-    <?php while(have_posts()): the_post();?>
-      <div class="row">
-        <article class="col-11 col-centered">
-          <?php the_content();?>
-        </article>
-      </div>
-    <?php endwhile;?>
-  </section>
-<?php endif;?>
 
 <?php
 // Include Newsletter Part
