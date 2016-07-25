8b295bfca659bbaa9b56963a7b85260102efafd5
mattada <mattadams@factor1studios.com>
2016-07-25 08:56:39 -0700

    Try to drop the aspect ration on ticket prices
    Per issue #151
    
    Signed-off-by: mattada <mattadams@factor1studios.com>

--- parts/tickets.php
+++ parts/tickets.php
@@ -1,26 +1,26 @@
 <div class="row" style="margin: 40px auto;">
-				<div class="col-3 priceblock first">
+				<div class="col-3 priceblock first stretch">
 					<div class="content">
 					<p class="title">AIGA Member<br>General Admission</p>
 					<p class="price">$199</p>
 					</div>
 				</div>
 
-				<div class="col-3 priceblock second">
+				<div class="col-3 priceblock second stretch">
 					<div class="content">
 					<p class="title">AIGA Non-Member<br>General Admission</p>
 					<p class="price">$269</p>
 					</div>
 				</div>
 
-				<div class="col-3 priceblock third">
+				<div class="col-3 priceblock third stretch">
 					<div class="content">
 					<p class="title">AIGA Member<br>Student Admission</p>
 					<p class="price">$99</p>
 					</div>
 				</div>
 
-				<div class="col-3 priceblock fourth">
+				<div class="col-3 priceblock fourth stretch">
 					<div class="content">
 					<p class="title">AIGA Non-Member<br>Student Admission</p>
 					<p class="price">$129</p>
