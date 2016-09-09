c38e20b3a05c6c81584dbfd51d0420cc20a5c7bb
mattada <mattadams@factor1studios.com>
2016-09-08 20:43:50 -0700

    create mm_program page template and baseline HTML
    Signed-off-by: mattada <mattadams@factor1studios.com>

---
+++ templates/mm_schedule.php
@@ -0,0 +1,127 @@
+<?php get_header();
+
+//Template Name: MM Schedule Page
+
+?>
+
+
+<?php
+//============ ** ============ //
+// 			Page HERO
+//============ ** ============ //?>
+
+<section class="hero mmconf_hero container">
+	<div class="row hero-content">
+		<div class="col-10 col-centered text-center">
+			<h1 class="text-white">
+				Method + Madness Program
+			</h1>
+			<h3 class="text-white">
+				October 22-­23, 2016
+				<br>The Phoenix Convention Center
+				<br>8am Check­in • 9am Talks begin
+			</h3>
+		</div>
+	</div>
+</section>
+
+<?php
+//================= ** ====================== //
+//  Second Row, Daily content schedule blocks
+//================= ** ====================== //
+?>
+
+
+<section class="container conf-program">
+	<div class="row">
+	<article class="col-11 col-centered">
+		<h2>Saturday, October 22</h2>
+		
+		<ul class="daily-details">	
+
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+		</ul>
+	</article>
+	
+	
+	<article class="col-11 col-centered">
+		<h2>Sunday, October 23</h2>
+		
+		<ul class="daily-details">	
+
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+			<li>
+				<div class="col-4">Time</div>
+				<div class="col-8">Detail</div>
+			</li>
+		</ul>
+	</article>
+	
+	
+		
