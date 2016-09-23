a300f8f16e1e13ad0f9aec15d830b5aa798aad27
mattada <mattadams@factor1studios.com>
2016-09-23 08:28:05 -0700

    set up logic for taxonomy single
    Signed-off-by: mattada <mattadams@factor1studios.com>

--- single-f1_staffgrid_cpt.php
+++ single-f1_staffgrid_cpt.php
@@ -26,12 +26,12 @@
 </section>
 
 
-
+<?php if('f1_staffgrid_tax' == 'main-stage') : ?>
 
 <?php
-//================= ** ================= //
-//  Second Row, Event repeaters block
-//================= ** ================ //
+//===================== ** ===================== //
+//  Second Row, Main Stage Speaker details block
+//===================== ** ==================== //
 ?>
 
 <section class="container speaker-top">
@@ -75,6 +75,49 @@
 
 
 </section>
+
+<?php if('f1_staffgrid_tax' == 'breakout') : ?>
+
+<?php
+//===================== ** ===================== //
+//  Second Row, Breakout Speaker details block
+//===================== ** ==================== //
+?>
+
+<section class="container speaker-top">
+	<div class="row">
+		<div class="col-6 stretch nopadding">
+			<?php if(has_post_thumbnail()) {
+			the_post_thumbnail('large');
+			} else {	}
+			?>	
+		</div>
+		
+		<div class="col-6 stretch nopadding">
+			<div class="staffbio">
+				<?php the_field('staff_bio');?>				
+			</div>
+			
+			<div class="f1_mentor_details_social_container">
+			<h3 class="text-teal">Find them online</h3>
+				<?php if(get_field( "twitter_url" )) : echo('<a href="'); the_field( "twitter_url" ); echo('"><i class="fa fa-twitter" aria-hidden="true"></i></a>');  endif; ?>
+				<?php if(get_field( "facebook_url" )) : echo('<a href="'); the_field( "facebook_url" ); echo('"><i class="fa fa-facebook" aria-hidden="true"></i></a>');  endif; ?>
+				<?php if(get_field( "instagram_url" )) : echo('<a href="'); the_field( "instagram_url" ); echo('"><i class="fa fa-instagram" aria-hidden="true"></i></a>');  endif; ?>
+				<?php if(get_field( "url" )) : echo('<a href="'); the_field( "url" ); echo('"><i class="fa fa-globe" aria-hidden="true"></i></a>');  endif; ?>
+			</div>
+
+		</div>
+	</div>
+	
+
+</section>
+
+
+
+<?php endif; ?>
+
+
+
 
 
 
