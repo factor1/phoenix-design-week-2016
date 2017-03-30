5f6e828a78f55f7e1c850adefc8acd548dcee676
Matt Adams <mattadams@factor1studios.com>
2017-03-30 10:41:32 -0700

    2017 temp page additions

---
+++ templates/soon2017.php
@@ -0,0 +1,67 @@
+<?php 
+	
+	// Template Name: 2017 soon
+	
+ wp_head(); ?>
+
+
+<html>
+	<head>
+		<title>Phoenix Design Week 2017 - Coming Soon</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link type="text/css" rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/tempassets/css/ginger.min.css">
+		<link type="text/css" rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/tempassets/css/theme.css">
+		<script src="https://use.fontawesome.com/71eb3125bb.js"></script>
+		
+		
+	</head>
+	
+	<body>
+		<div class="container">
+			
+			
+		<div class="row">
+			<div class="col-12">
+				<h1>Phoenix Design Week 2017 - COMING SOON</h1>
+				<h2>October 20-27, 2017</h2>
+				<P>Presented by:<br></p>
+				<img src="<?php bloginfo('template_url'); ?>/tempassets/images/aiga_az.png" alt="aiga_az" width="400px" />
+			</div>
+		
+		</div>
+		
+
+		<div class="row">
+			
+			<div class="col-8" style="padding-right: 80px;">
+				<h2>About PHXDW 2017</h2>
+				<p>Every year, Phoenix Design Week brings together hundreds of creative professionals from across the Southwest for a weeklong celebration of design, a series of presentations, workshops, and social events, all curated to inspire, strengthen, and showcase the Arizona design community.</p>
+
+<p>The highlight of Phoenix Design Week is the Method + Madness Conference. Attendees will get two full days of actionable insights from leading professionals, as well as hands on education and massive amounts of fiery inspiration.</p>
+
+<a href="http://phxdw.com/home" class="">Visit the 2016 site</a>
+
+
+			</div>
+			
+			
+			
+			<div class="col-4">
+				<h2>Get Involved</h2>
+				<p>We need you help for volunteers, sponsors, events, and speakers. </p>
+				<a href="mailto:president@arizona.aiga.org" class="button">Contact us</a>
+				
+				
+
+			</div>
+
+	
+			
+		</div>
+		</div>
+		</body>
+		</html>
+		
+		
+		<?php wp_footer();?>
